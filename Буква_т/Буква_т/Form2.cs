117172bc9189c495d28695c775405be604f4123c
catran97 <artur.tikhonov.97@mail.ru>
2018-10-10 08:55:05 +0500

    Окончательная версия
    Программа определяет колличество букв "т" в тексте.

---
+++ Буква_т/Буква_т/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Буква_т
+{
+    public partial class Form2 : Form
+    {
+        public Form2(int n)
+        {
+            InitializeComponent();
+            this.n = n;
+            label1.Text = "Количество букв т " + n;
+        }
+        int n;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
